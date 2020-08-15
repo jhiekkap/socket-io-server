2d3375bdb049a7c59bcfac9e4f3890cceaa4d66d
jhiekkap <jari.hiekkapelto@gmail.com>
2020-08-15 17:00:02 +0300

    klkl

--- app.js
+++ app.js
@@ -15,21 +15,25 @@ const io = socketIo(server);
 let interval;
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
+    console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on('chat message', function (msg) {
+        console.log(msg)
+       io.emit('chat message', msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    });
 });
 
 const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
+    const response = new Date();
+    // Emitting a new message. Will be consumed by the client
+    socket.emit("FromAPI", response);
 };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
