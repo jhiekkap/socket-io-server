1673909b7e999f4e9970eb052f31c430f5a6d4c9
jhiekkap <jari.hiekkapelto@gmail.com>
2020-08-15 22:32:08 +0300

    klklk

---
+++ build/precache-manifest.f4eee70059b62c88395da1f48569f89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafbbe707587cee4b6a88d41ec9503ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00bce5c1b5853bb0901",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c4a26bbe2cf2510e579",
+    "url": "/static/js/2.21e69156.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.21e69156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00bce5c1b5853bb0901",
+    "url": "/static/js/main.c0e27448.chunk.js"
+  },
+  {
+    "revision": "42a746f39177499e55df",
+    "url": "/static/js/runtime-main.7914c254.js"
+  }
+]);
