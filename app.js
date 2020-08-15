03c2cb3b9ff5ec5acb8b70d8080251354bc101c7
jhiekkap <jari.hiekkapelto@gmail.com>
2020-08-15 17:38:42 +0300

    eka

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ io.on("connection", (socket) => {
     if (interval) {
         clearInterval(interval);
     }
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    //interval = setInterval(() => getApiAndEmit(socket), 1000);
     socket.on('chat message', function (msg) {
         console.log(msg)
        io.emit('chat message', msg);
